484c4c66d2e275a2bd7e173c468096adaf616dd6
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2023-05-12 23:00:19 +0530

    Add files via upload

---
+++ Question 66 Plus One/src/plus.java
@@ -0,0 +1,22 @@
+public class plus {
+    static int[] plusOne(int[] digits){
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
+                digits[i]++; return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3};
+        plusOne(arr);
+        for (int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
