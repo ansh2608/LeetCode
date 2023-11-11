f406439e31a4dc10f5fb7d24711671ba68e9e0d0
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2023-11-11 14:31:11 +0530

    Add files via upload

---
+++ Question 2642 Design Graph With Shortest Path Calculator/src/Shortest_Path_Calculator.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Shortest_Path_Calculator {
+    List<List<Pair<Integer, Integer>>> g;
+    public Graph(int n, int[][] edges) {
+        g = new ArrayList<>(n);
+        for(int i = 0; i < n; ++i) g.add(new ArrayList<>());
+        for(int[] e: edges) addEdge(e);
+    }
+
+    public void addEdge(int[] edge) {
+        g.get(edge[0]).add(new Pair<>(edge[1], edge[2]));
+    }
+    public int shortestPath(int node1, int node2) {
+        int minPath = -1;
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> vis = new ArrayList<>(Collections.nCopies(g.size(), Integer.MAX_VALUE));
+        q.add(node1);
+        vis.set(node1, 0);
+        while(!q.isEmpty()){
+            int node = q.poll();
+            if(node == node2) minPath = vis.get(node);
+            for(Pair<Integer, Integer> pair: g.get(node)){
+                int n = pair.getKey(), c = pair.getValue();
+                if(vis.get(n) < vis.get(node) + c) continue;
+                q.add(n);
+                vis.set(n, vis.get(node) + c);
+            }
+        }
+        return minPath;
+    }
+    public static void main(String[] args) {
+
+    }
+}
