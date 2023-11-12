94951ec621829a68ed291fcdb8d812f3f5378668
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2023-11-12 14:47:01 +0530

    Add files via upload

---
+++ Question 815 Bus Routes/src/BusRoutes.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class BusRoutes {
+    static int numBusesToDestination(int[][] routes, int source, int target){
+        if (source == target) return 0;
+        int maxStop = -1;
+        for (int[] route : routes) {
+            for (int stop : route) maxStop = Math.max(maxStop, stop);
+        }
+        if (maxStop < target) return -1;
+        int n = routes.length;
+        int[] minBusesToReach = new int[maxStop + 1];
+        Arrays.fill(minBusesToReach, n + 1);
+        minBusesToReach[source] = 0;
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            for (int[] route : routes) {
+                int min = n + 1;
+                for (int stop : route) min = Math.min(min, minBusesToReach[stop]);
+                min++;
+                for (int stop : route) {
+                    if (minBusesToReach[stop] > min) {
+                        minBusesToReach[stop] = min;
+                        flag = true;
+                    }
+                }
+            }
+
+        }
+        return (minBusesToReach[target] < n + 1 ? minBusesToReach[target] : -1);
+    }
+    public static void main(String[] args) {
+        int[][] routes = {{1,2,7},{3,6,7}};
+        int source = 1, target = 6;
+        System.out.println(numBusesToDestination(routes,source,target));
+    }
+}
