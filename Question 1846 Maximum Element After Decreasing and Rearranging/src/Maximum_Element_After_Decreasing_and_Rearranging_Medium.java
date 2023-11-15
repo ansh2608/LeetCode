bcba66a587fa14480aa2280e062fa6118146f97b
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2023-11-15 16:28:02 +0530

    Add files via upload

---
+++ Question 1846 Maximum Element After Decreasing and Rearranging/src/Maximum_Element_After_Decreasing_and_Rearranging_Medium.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Maximum_Element_After_Decreasing_and_Rearranging_Medium {
+    static int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0]=1;
+        for (int i=1;i<arr.length;i++){
+            if (Math.abs(arr[i] - arr[i - 1]) <= 1) continue;
+            else arr[i] = arr[i-1]+1;
+        }
+        return arr[arr.length-1];
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,2,1,2,1};
+        System.out.println(maximumElementAfterDecrementingAndRearranging(arr));
+    }
+}
