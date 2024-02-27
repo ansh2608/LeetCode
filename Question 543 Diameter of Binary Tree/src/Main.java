51122cbf5a0f4d1f6b005bad4e12e2dbe6034ef1
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-02-27 22:20:23 +0530

    Add files via upload

--- Question 543 Diameter of Binary Tree/src/Main.java
+++ Question 543 Diameter of Binary Tree/src/Main.java
@@ -1,4 +1,25 @@
 public class Main {
+     public static class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+         TreeNode(int val) {
+             this.val = val;
+         }
+     }
+    public int height(TreeNode root){
+        if(root==null || root.left==null && root.right==null) return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root==null || (root.left==null && root.right==null)) return 0;
+        int left=diameterOfBinaryTree(root.left);
+        int right=diameterOfBinaryTree(root.right);
+        int mid=height(root.left)+height(root.right);
+        if (root.left!=null) mid++;
+        if (root.right!=null) mid++;
+        return Math.max(mid,Math.max(left,right));
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
