eae125d3244f5fd0737d306d86e571d14b52539a
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-02-28 12:56:56 +0530

    Add files via upload

--- Question 513 Find Bottom Left Tree Value/src/Main.java
+++ Question 513 Find Bottom Left Tree Value/src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
+    static int maxDepth=0,ans=0;
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val; }
+    }
+    public static void dfs(TreeNode root,int depth){
+        if (root==null) return;
+        if (root.left==null && root.right==null && depth>maxDepth){
+            ans=root.val;
+            maxDepth=depth;
+        }
+        dfs(root.left,depth+1);
+        dfs(root.right,depth+1);
+    }
+    public static int findBottomLeftValue(TreeNode root) {
+        dfs(root,1);
+        return ans;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        TreeNode root=new TreeNode(2);
+        TreeNode a=new TreeNode(1);
+        TreeNode b=new TreeNode(3);
+        root.left=a;
+        root.right=b;
+        System.out.println(findBottomLeftValue(root));
     }
 }
