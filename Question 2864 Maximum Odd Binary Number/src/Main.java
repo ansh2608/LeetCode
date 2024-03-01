54fe599ec1f86d672e3a85ea2d82ffbeda95545e
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-03-01 16:27:13 +0530

    Add files via upload

---
+++ Question 2864 Maximum Odd Binary Number/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static String maximumOddBinaryNumber(String s) {
+        int cnt1 = 0, cnt0 = 0;
+        for(char a:s.toCharArray()){
+            if(a=='1') cnt1++;
+            else if(a=='0') cnt0++;
+        }
+        StringBuilder ans = new StringBuilder();
+        ans.append("1".repeat(cnt1-1));
+        ans.append("0".repeat(cnt0));
+        ans.append("1");
+        return ans.toString();
+    }
+    public static void main(String[] args) {
+        String s = "010";
+        System.out.println(maximumOddBinaryNumber(s));
+    }
+}
