804852cf4cb000e73eecb727e92864722b54adfb
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-03-29 21:52:50 +0530

    Add files via upload

---
+++ Question 2962 Count Subarrays Where Max Element Appears at Least K Times/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static long countSubarrays(int[] nums, int k) {
+        long maxNum = Long.MIN_VALUE, count = 0;
+        long left = 0, right = 0, ans = 0;
+        for (int num : nums) maxNum = Math.max(maxNum, num);
+        while (right < nums.length || left > right) {
+            if (nums[(int)right] == maxNum) count++;
+            while (count >= k) {
+                if (nums[(int)left] == maxNum) count--;
+                left++;
+                ans += nums.length - right;
+            }
+            right++;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,3,2,3,3};
+        int k = 2;
+        System.out.println(countSubarrays(nums,k));
+    }
+}
