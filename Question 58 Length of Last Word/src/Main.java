d37b827220983cb608e9d8f5c64beafef3118914
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-04-01 13:38:18 +0530

    Add files via upload

---
+++ Question 58 Length of Last Word/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static int lengthOfLastWord(String s) {
+        s = s.trim();
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') length++;
+            else if (length > 0) break;
+        }
+        return length;
+    }
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(lengthOfLastWord(s));
+    }
+}
