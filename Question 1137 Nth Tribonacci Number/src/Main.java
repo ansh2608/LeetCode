a94ce2dbf552ee0e7c63a535971f08f2984bb1f5
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-04-24 19:42:33 +0530

    Add files via upload

---
+++ Question 1137 Nth Tribonacci Number/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static int tribonacci(int n) {
+        if (n < 2) return n;
+        int[] dp = {0, 1, 1};
+        for (int i = 3; i <= n; ++i) {
+            final int next = dp[0] + dp[1] + dp[2];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = next;
+        }
+        return dp[2];
+    }
+    public static void main(String[] args) {
+        System.out.println(tribonacci(4));
+    }
+}
