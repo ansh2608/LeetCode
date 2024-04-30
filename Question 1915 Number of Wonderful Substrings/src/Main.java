8a8b7a1f2087cad4a95889cb3932c51b60de7350
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-04-30 21:59:34 +0530

    Add files via upload

---
+++ Question 1915 Number of Wonderful Substrings/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static long wonderfulSubstrings(String word) {
+        long[] count = new long[1024];
+        long result = 0;
+        int prefixXor = 0;
+        count[prefixXor] = 1;
+        for (char ch : word.toCharArray()) {
+            int charIndex = ch - 'a';
+            prefixXor ^= 1 << charIndex;
+            result += count[prefixXor];
+            for (int i = 0; i < 10; i++) result += count[prefixXor ^ (1 << i)];
+            count[prefixXor]++;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        String word = "aba";
+        System.out.println(wonderfulSubstrings(word));
+    }
+}
