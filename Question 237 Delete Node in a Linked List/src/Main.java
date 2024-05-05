867e7a4dcf63f2f055133e31c12f98663ce30aac
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-05-05 13:58:42 +0530

    Add files via upload

--- Question 237 Delete Node in a Linked List/src/Main.java
+++ Question 237 Delete Node in a Linked List/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
+    public static class ListNode {
+       int val;
+       ListNode next;
+       ListNode(int x) {
+           val = x;
+       }
+   }
+    public static void deleteNode(ListNode node) {
+        node.val=node.next.val;
+        node.next=node.next.next;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ListNode head=new ListNode(4);
+        ListNode a=new ListNode(5);
+        ListNode b=new ListNode(1);
+        ListNode c=new ListNode(9);
+        head.next=a;
+        a.next=b;
+        b.next=c;
+        ListNode node=new ListNode(5);
+        deleteNode(node);
+        while (head!=null){
+            System.out.print(head.val+" ");
+            head=head.next;
+        }
     }
 }
