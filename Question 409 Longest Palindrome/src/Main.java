43a68a60bb1a5d254a51ff3f2ce54eb411e649fe
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-04 19:05:52 +0530

    Add files via upload

---
+++ Question 409 Longest Palindrome/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Main {
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (hm.containsKey(c)) hm.put(c, hm.get(c) + 1);
+            else hm.put(c, 1);
+        }
+        int ans = 0;
+        boolean isFirstOdd = false;
+        for (Character key : hm.keySet()) {
+            if (hm.get(key) % 2 == 0) ans += hm.get(key);
+            else {
+                ans += hm.get(key);
+                if (!isFirstOdd) isFirstOdd = true;
+                else ans -= 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        String s = "abccccdd";
+        System.out.println("Hello world!");
+    }
+}
