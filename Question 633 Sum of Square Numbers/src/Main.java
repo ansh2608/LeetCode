87ae3022f0a0b344fa9977fb7664a219767b5845
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-17 11:51:00 +0530

    Add files via upload

---
+++ Question 633 Sum of Square Numbers/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static boolean judgeSquareSum(int c) {
+        for (int divisor = 2; divisor * divisor <= c; divisor++) {
+            if (c % divisor == 0) {
+                int exponentCount = 0;
+                while (c % divisor == 0) {
+                    exponentCount++;
+                    c /= divisor;
+                }
+                if (divisor % 4 == 3 && exponentCount % 2 != 0) {
+                    return false;
+                }
+            }
+        }
+        return c % 4 != 3;
+    }
+    public static void main(String[] args) {
+        int c=3;
+        System.out.println(judgeSquareSum(c));
+    }
+}
