650284b22cad0a8942acc52efebc2a9b973ee478
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-06-25 22:06:25 +0530

    Add files via upload

---
+++ Question 1038 Binary Search Tree to Greater Sum Tree/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+    }
+    private int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);  // Traverse the right subtree
+            sum += root.val;  // Update the sum
+            root.val = sum;  // Update the current node's value
+            bstToGst(root.left);  // Traverse the left subtree
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
