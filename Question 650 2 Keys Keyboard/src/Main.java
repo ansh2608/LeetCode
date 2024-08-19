15407f461ced0b3a6c91f6df2c011a06f26de198
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-08-19 21:06:38 +0530

    Add files via upload

---
+++ Question 650 2 Keys Keyboard/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public int minSteps(int n) {
+        if (n == 1) return 0;
+        int steps = 0;
+        int factor = 2;
+        while (n > 1) {
+            while (n % factor == 0) {
+                steps += factor;
+                n /= factor;
+            }
+            factor++;
+        }
+        return steps;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
