98a72165e9f1b5a6a5919876e82601370c39132b
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-02 18:44:49 +0530

    Add files via upload

---
+++ Question 1894 Find the Student that Will Replace the Chalk/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public int chalkReplacer(int[] chalk, int initialChalkPieces) {
+        long totalChalkNeeded = 0;
+        for (int studentChalkUse : chalk) totalChalkNeeded += studentChalkUse;
+        int remainingChalk = (int)(initialChalkPieces % totalChalkNeeded);
+        for (int studentIndex = 0; studentIndex < chalk.length; studentIndex++) {
+            if (remainingChalk < chalk[studentIndex]) return studentIndex;
+            remainingChalk -= chalk[studentIndex];
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
