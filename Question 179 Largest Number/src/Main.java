db1ace555dafe456178fd1525450406d7c189009
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-18 21:20:00 +0530

    Add files via upload

---
+++ Question 179 Largest Number/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class Main {
+    public String largestNumber(int[] nums) {
+        String[] array =  new String[nums.length];
+        for(int i=0; i<nums.length; i++) array[i] = String.valueOf(nums[i]);
+        Arrays.sort(array,(a, b)-> (b+a).compareTo(a+b));
+        if(array[0].equals("0")) return "0";
+        StringBuilder largest = new StringBuilder();
+        for(int i=0; i<array.length; i++) largest.append(array[i]);
+        return largest.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
