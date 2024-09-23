690e059a9b5caeeda42a07c09ea2d3c2c0cc80af
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-23 13:31:00 +0530

    Add files via upload

---
+++ Question 2707 Extra Characters in a String/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public int minExtraChar(String s, String[] dictionary) {
+        Set<String> dict = new HashSet<>();
+        for (String word : dictionary) dict.add(word);
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++) dp[i] = n;
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                String sub = s.substring(j, i);
+                if (dict.contains(sub)) dp[i] = Math.min(dp[i], dp[j]);
+            }
+            dp[i] = Math.min(dp[i], dp[i - 1] + 1);
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
