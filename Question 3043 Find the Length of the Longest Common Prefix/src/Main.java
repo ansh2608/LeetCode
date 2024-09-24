8d225f636d30b4fad8ce77ed669936deeb7b825e
Ansh Porwal <98257000+ansh2608@users.noreply.github.com>
2024-09-24 10:38:01 +0530

    Add files via upload

---
+++ Question 3043 Find the Length of the Longest Common Prefix/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Main {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        HashMap<String, Integer> prefixMap = new HashMap<>();
+        for (int num : arr1) {
+            String strNum = Integer.toString(num);
+            String prefix = "";
+            for (char ch : strNum.toCharArray()) {
+                prefix += ch;
+                prefixMap.put(prefix, prefixMap.getOrDefault(prefix, 0) + 1);
+            }
+        }
+        int maxLength = 0;
+        for (int num : arr2) {
+            String strNum = Integer.toString(num);
+            String prefix = "";
+            for (char ch : strNum.toCharArray()) {
+                prefix += ch;
+                if (prefixMap.containsKey(prefix)) maxLength = Math.max(maxLength, prefix.length());
+            }
+        }
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
